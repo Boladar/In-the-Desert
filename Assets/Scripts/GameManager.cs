7126a45dc49ca29a23d9d3cd28fdc5d8d70550de
Boladar <aleksander.szamalek@gmail.com>
2016-07-09 00:56:50 +0200

    1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private BoardManager boardScript;
+
+
+	// Use this for initialization
+	void Start () {
+		boardScript = GetComponent<BoardManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
