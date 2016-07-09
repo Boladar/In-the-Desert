99a6bc31df57ecef658ea3366a5a78c1f6265a63
Boladar <aleksander.szamalek@gmail.com>
2016-07-09 20:47:49 +0200

    gun rotation

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -14,31 +14,48 @@ public class Controls : MonoBehaviour {
 	void FixedUpdate () {
 		//up
 		if (Input.GetKey (KeyCode.W)) {
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up, verticalCheck*speed * Time.deltaTime, blockingLayer);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up, verticalCheck * speed * Time.deltaTime, blockingLayer);
 			if (hit.collider == null) {
 				transform.position += Vector3.up * speed * Time.deltaTime;
 			}
 		}
 		//right
 		if (Input.GetKey (KeyCode.D)) {
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.right, horizontalCheck*speed * Time.deltaTime, blockingLayer);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.right, horizontalCheck * speed * Time.deltaTime, blockingLayer);
 			if (hit.collider == null) {
 				transform.position += Vector3.right * speed * Time.deltaTime;
 			}
 		}
 		//left
 		if (Input.GetKey (KeyCode.A)) {
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.left, horizontalCheck*speed * Time.deltaTime, blockingLayer);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.left, horizontalCheck * speed * Time.deltaTime, blockingLayer);
 			if (hit.collider == null) {
 				transform.position += Vector3.left * speed * Time.deltaTime;
 			}
 		}
 		//down
 		if (Input.GetKey (KeyCode.S)) {
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.down, verticalCheck*speed * Time.deltaTime, blockingLayer);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.down, verticalCheck * speed * Time.deltaTime, blockingLayer);
 			if (hit.collider == null) {
 				transform.position += Vector3.down * speed * Time.deltaTime;
 			}
 		}
+	}
+
+	Vector3 mousePos;
+	public Transform gun;
+	Vector3 objectPos;
+	float angle;
+
+	void Update(){
+		mousePos = Input.mousePosition;
+
+		objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+
+		angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		gun.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+
 	}
 }
