99af392f91541c14f02b06926f08b73e7cf60c05
Boladar <aleksander.szamalek@gmail.com>
2016-07-13 16:23:10 +0200

    looseitem 0.54

--- Assets/Scripts/LooseItem.cs
+++ Assets/Scripts/LooseItem.cs
@@ -5,19 +5,37 @@ public class LooseItem : MonoBehaviour {
 
 	public Item item;
 	public int amount;
+	public float detectDistance;
+	public LayerMask playerMask;
 
-	private Inventory inv;
+	private GameObject player;
+	private Inventory inventory;
 
 	void Start(){
-		inv = GameObject.Find ("Inventory").GetComponent<Inventory>();
+		inventory = GameObject.Find ("Inventory").GetComponent<Inventory>();
+		player = GameObject.Find ("Character");
 	}
 
-	void OnTriggerStay2D(Collider2D other){
+	/*void OnTriggerStay2D(Collider2D other){
 		if (other.gameObject.tag == "Player" && Input.GetKey (KeyCode.E)) {
 			Debug.Log ("pickup, ID: " + item.ID + ", amount: " + amount);
 			inv.AddItem (item.ID, amount);	
 			Destroy (this.gameObject);
 		}
+	}*/
+
+	void Update(){
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, player.transform.position - this.transform.position, detectDistance, playerMask);
+		if (hit != null) {
+			player.GetComponent<LooseItemTooltip> ().Activate (item);	
+
+			if (Input.GetKey (KeyCode.E)) {
+				Debug.Log ("pickup, ID: " + item.ID + ", amount: " + amount);
+				inventory.AddItem (item.ID, amount);	
+				Destroy (this.gameObject);
+			}
+		}
 	}
+
 		
 }
---
+++ Assets/Scripts/LooseItemTooltip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LooseItemTooltip : MonoBehaviour {
+
+	private Item item;
+	private string data;
+	private GameObject tooltip;
+
+	void Start(){
+		tooltip = GameObject.Find ("LooseItemTooltip");
+		tooltip.SetActive (false);
+	}
+
+	void Update(){
+		if (tooltip.activeSelf) {
+			tooltip.transform.position = this.transform.position;
+		}
+	}
+
+	public void Activate(Item item){
+		this.item = item;
+		ConstructDataString ();
+		tooltip.SetActive (true);
+	}
+
+	public void Deactivate(){
+		tooltip.SetActive (false);
+	}
+
+	public void ConstructDataString(){
+		data = "<color=#000000>" + item.Title + "</color> \n\n" + item.Description + "\n" + item.Value;
+		tooltip.transform.GetChild (0).GetComponent<Text> ().text = data;
+	}
+}
